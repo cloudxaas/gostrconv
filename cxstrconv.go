2ec2185016980fd21b6ff2f88f488ae2589f9217
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-03-23 08:21:54 -0700

    Create cxstrconv.go

---
+++ cxstrconv.go
@@ -0,0 +1,15 @@
+package cxstrconv
+  
+import (
+    "fmt"
+    "strconv"
+    "github.com/cloudxaas/gocx"
+)
+  
+func CXstrconv3BytesToUint16(in string) uint16 {
+    return uint16(strconv.FormatUint16(in,10)
+}
+                  
+func CXstrconvUint16To3Bytes(in uint16) string {
+  return strconv.ParseUint16(in,36)
+}
