7782d9faf7a9589be6b3181698ec8dfe3323477b
Kolin Fluence <kolinfluence@gmail.com>
2023-04-06 23:31:15 -0700

    123

--- cxstrconv.go
+++ cxstrconv.go
@@ -1,121 +1,126 @@
 package cxstrconv
 
-func Int8toa(n int8) []byte {
-    if n == 0 {
-        return []byte{'0'}
-    }
+import (
+	cx "github.com/cloudxaas/gocx"
+)
 
-    buf := make([]byte, 0, 3)
-    neg := false
 
-    if n < 0 {
-        neg = true
-        n = -n
-    }
+func Int8toa(n int8) string {
+	if n == 0 {
+		return "0"
+	}
 
-    for n > 0 {
-        buf = append(buf, byte(n%10)+'0')
-        n /= 10
-    }
+	var buf [4]byte // max int8 string length is 3 digits plus the sign
+	i := len(buf) - 1
+	neg := false
 
-    if neg {
-        buf = append(buf, '-')
-    }
+	if n < 0 {
+		neg = true
+		n = -n
+	}
 
-    for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
-        buf[i], buf[j] = buf[j], buf[i]
-    }
+	for n > 0 {
+		buf[i] = byte(n%10) + '0'
+		n /= 10
+		i--
+	}
 
-    return buf
+	if neg {
+		buf[i] = '-'
+	} else {
+		i++
+	}
+
+	return cx.B2s(buf[i:])
 }
 
-func Int16toa(n int16) []byte {
-    if n == 0 {
-        return []byte{'0'}
-    }
 
-    buf := make([]byte, 0, 5)
-    neg := false
+func Int16toa(n int16) string {
+	if n == 0 {
+		return "0"
+	}
 
-    if n < 0 {
-        neg = true
-        n = -n
-    }
+	var buf [6]byte // max int16 string length is 5 digits plus the sign
+	i := len(buf) - 1
+	neg := false
 
-    for n > 0 {
-        buf = append(buf, byte(n%10)+'0')
-        n /= 10
-    }
+	if n < 0 {
+		neg = true
+		n = -n
+	}
 
-    if neg {
-        buf = append(buf, '-')
-    }
+	for n > 0 {
+		buf[i] = byte(n%10) + '0'
+		n /= 10
+		i--
+	}
 
-    for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
-        buf[i], buf[j] = buf[j], buf[i]
-    }
+	if neg {
+		buf[i] = '-'
+	} else {
+		i++
+	}
 
-    return buf
+	return cx.B2s(buf[i:])
 }
 
-func Int32toa(n int32) []byte {
-    if n == 0 {
-        return []byte{'0'}
-    }
+func Int32toa(n int32) string {
+	if n == 0 {
+		return "0"
+	}
 
-    buf := make([]byte, 0, 11)
-    neg := false
+	var buf [11]byte // max int32 string length is 10 digits plus the sign
+	i := len(buf) - 1
+	neg := false
 
-    if n < 0 {
-        neg = true
-        n = -n
-    }
+	if n < 0 {
+		neg = true
+		n = -n
+	}
 
-    for n > 0 {
-        buf = append(buf, byte(n%10)+'0')
-        n /= 10
-    }
+	for n > 0 {
+		buf[i] = byte(n%10) + '0'
+		n /= 10
+		i--
+	}
 
-    if neg {
-        buf = append(buf, '-')
-    }
+	if neg {
+		buf[i] = '-'
+	} else {
+		i++
+	}
 
-    for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
-        buf[i], buf[j] = buf[j], buf[i]
-    }
-
-    return buf
+	return cx.B2s(buf[i:])
 }
 
-func Int64toa(n int64) []byte {
-    if n == 0 {
-        return []byte{'0'}
-    }
+func Int64toa(n int64) string {
+	if n == 0 {
+		return "0"
+	}
 
-    buf := make([]byte, 0, 20)
-    neg := false
+	var buf [20]byte // max int64 string length is 19 digits plus the sign
+	i := len(buf) - 1
+	neg := false
 
-    if n < 0 {
-        neg = true
-        n = -n
-    }
+	if n < 0 {
+		neg = true
+		n = -n
+	}
 
-    for n > 0 {
-        buf = append(buf, byte(n%10)+'0')
-        n /= 10
-    }
+	for n > 0 {
+		buf[i] = byte(n%10) + '0'
+		n /= 10
+		i--
+	}
 
-    if neg {
-        buf = append(buf, '-')
-    }
+	if neg {
+		buf[i] = '-'
+	} else {
+		i++
+	}
 
-    for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
-        buf[i], buf[j] = buf[j], buf[i]
-    }
-
-    return buf
+	return cx.B2s(buf[i:])
 }
-
 
 func Uint8toa(n uint8) string {
     if n == 0 {
@@ -128,51 +133,51 @@ func Uint8toa(n uint8) string {
         buf[i] = '0' + n%10
         n /= 10
     }
-    return string(buf[i:])
+    return cx.B2s(buf[i:])
 }
 
 func Uint16toa(n uint16) string {
-    if n == 0 {
-        return "0"
-    }
-    
-    var b [5]byte
-    i := len(b) - 1
-    for n > 0 {
-        b[i] = byte(n%10) + '0'
-        n /= 10
-        i--
-    }
-    
-    return string(b[i+1:])
+	if n == 0 {
+		return "0"
+	}
+
+	var b [5]byte
+	i := len(b) - 1
+	for n > 0 {
+		b[i] = byte(n%10) + '0'
+		n /= 10
+		i--
+	}
+
+	return cx.B2s(b[i+1:])
+}
+func Uint32toa(n uint32) string {
+	if n == 0 {
+		return "0"
+	}
+	buf := [11]byte{} // max uint32 string length is 10 digits
+	i := len(buf) - 1
+	for n > 0 {
+		buf[i] = byte(n%10) + '0'
+		n /= 10
+		i--
+	}
+	return cx.B2s(buf[i+1:])
 }
 
-func Uint32toa(n uint32) string {
-    if n == 0 {
-        return "0"
-    }
-    buf := [11]byte{} // max uint32 string length is 10 digits
-    i := len(buf) - 1
-    for n > 0 {
-        buf[i] = byte(n%10) + '0'
-        n /= 10
-        i--
-    }
-    return string(buf[i+1:])
-}
 
 func Uint64toa(n uint64) string {
-    if n == 0 {
-        return "0"
-    }
-    buf := [20]byte{}
-    i := len(buf) - 1
-    for n > 0 {
-        q := n / 10
-        r := n % 10
-        buf[i] = byte(r + '0')
-        i--
-        n = q
-    }
-    return string(buf[i+1:])
+	if n == 0 {
+		return "0"
+	}
+	buf := [20]byte{} // max uint64 string length is 20 digits
+	i := len(buf) - 1
+	for n > 0 {
+		q := n / 10
+		r := n % 10
+		buf[i] = byte(r + '0')
+		i--
+		n = q
+	}
+	return cx.B2s(buf[i+1:])
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudxaas/gostrconv
+
+go 1.20
